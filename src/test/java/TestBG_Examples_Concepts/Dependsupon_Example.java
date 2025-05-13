4d937c99f3b37c1055ac63ef44ca3849c17d80c1
Shivanisohal <sohalshivani2@gmail.com>
2025-05-13 14:57:34 +0530

    TestNG Concepts- Grouping and Dependson, Priority

---
+++ src/test/java/TestBG_Examples_Concepts/Dependsupon_Example.java
@@ -0,0 +1,18 @@
+package TestBG_Examples_Concepts;
+
+import org.testng.annotations.Test;
+
+public class Dependsupon_Example {
+
+    @Test
+    public void serverTestOk()
+    {
+        System.out.println("I will run first");
+    }
+
+    @Test(dependsOnMethods = "serverTestOk")
+    public void Test1()
+    {
+        System.out.println("Testcase 1");
+    }
+}
---
+++ src/test/java/TestBG_Examples_Concepts/Priority_Example.java
@@ -0,0 +1,22 @@
+package TestBG_Examples_Concepts;
+
+import org.testng.annotations.Test;
+
+public class Priority_Example {
+
+    @Test(priority =2)
+    public void T1()
+    {
+        System.out.println("T2");
+    }
+    @Test(priority = 1)
+    public void T2()
+    {
+        System.out.println("T1");
+    }
+    @Test(priority =3)
+    public void T3()
+    {
+        System.out.println("T3");
+    }
+}
